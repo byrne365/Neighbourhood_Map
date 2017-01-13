17aa87e3873ef3617a36fef4f69fbf4f491f69f3
Brandon Byrne <byrnevideo@gmail.com>
2017-01-13 13:48:54 +1300

    Add files via upload

---
+++ js/brewerysDetails.js
@@ -0,0 +1,56 @@
+var brewerysDetails = [
+  {
+    'name': 'Hallertau Brewery',
+    'yelpID': 'hallertau-brewery-and-biergarten-auckland',
+    'lat': -36.766848,
+    'lng': 174.587941
+  },
+  {
+    'name': 'Shakespeare Hotel & Brewery',
+    'yelpID': 'the-shakespeare-brewery-auckland',
+    'lat': -36.846796,
+    'lng': 174.764036
+  },
+  {
+    'name': 'Brothers Brewery',
+    'yelpID':'brothers-brewery-auckland-2',
+    'lat': -36.868469,
+    'lng': 174.7588
+  },
+  {
+    'name': 'Wild on Waiheke',
+    'yelpID': 'wild-on-waiheke-waiheke-island',
+    'lat': -36.801094,
+    'lng': 175.065476
+  },
+  {
+    'name': 'Deep Creek Brews and Eats',
+    'yelpID': 'deep-creek-brews-and-eats-auckland',
+    'lat': -36.713628,
+    'lng': 174.748569
+  },
+  {
+    'name': 'Birkenhead Brewing Company',
+    'yelpID': 'birkenhead-brewing-company-birkenhead',
+    'lat': -36.811806,
+    'lng': 174.726483
+  },
+  {
+    'name':'Brothers Beer',
+    'yelpID': 'brothers-beer-auckland',
+    'lat': -36.850032,
+    'lng': 174.758561
+  },
+  {
+    'name':'The Brewers Co Operative',
+    'yelpID': 'brewers-co-operative-auckland',
+    'lat': -36.847833,
+    'lng': 174.759784
+  },
+  {
+    'name':'Cove Bites & Brews by Deep Creek Brewing Co',
+    'yelpID': 'cove-bites-and-brews-auckland',
+    'lat': -36.782462,
+    'lng': 175.009747
+  }
+]
